ea3147b9a7b549446a61f9fc00bcac203143d902
Arsh <arshgangani18@gmail.com>
2023-04-29 12:07:06 +0530

    Added user interface

---
+++ welcome.js
@@ -0,0 +1,50 @@
+const newTaskInput = document.getElementById("new-task");
+const addBtn = document.getElementById("add-btn");
+const todoList = document.getElementById("todo-list");
+
+function addTask() {
+    const newTaskText = newTaskInput.value;
+
+    if (newTaskText === "") {
+        return;
+    }
+
+    const newTask = document.createElement("li");
+    const checkbox = document.createElement("input");
+    checkbox.type = "checkbox";
+    checkbox.addEventListener("change", completeTask);
+
+    const text = document.createElement("span");
+    text.innerText = newTaskText;
+
+    const deleteBtn = document.createElement("button");
+    deleteBtn.innerText = "Delete";
+    deleteBtn.classList.add("delete-btn");
+    deleteBtn.addEventListener("click", deleteTask);
+
+    newTask.appendChild(checkbox);
+    newTask.appendChild(text);
+    newTask.appendChild(deleteBtn);
+    todoList.appendChild(newTask);
+
+    newTaskInput.value = "";
+}
+
+function completeTask(event) {
+    const checkbox = event.target;
+    const task = checkbox.parentElement;
+
+    if (checkbox.checked) {
+        task.classList.add("completed");
+    } else {
+        task.classList.remove("completed");
+    }
+}
+
+function deleteTask(event) {
+    const deleteBtn = event.target;
+    const task = deleteBtn.parentElement;
+    task.remove();
+}
+
+addBtn.addEventListener("click", addTask);
---
+++ welcome.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+    <title>To-Do List</title>
+    <link rel="stylesheet" type="text/css" href="style.css">
+</head>
+
+<body>
+    <div class="todo-container">
+        <h1>To-Do List</h1>
+        <input type="text" id="new-task" placeholder="Add a new task...">
+        <button id="add-btn">Add</button>
+        <ul id="todo-list"></ul>
+    </div>
+
+    <script src="script.js"></script>
+</body>
+
+</html>
