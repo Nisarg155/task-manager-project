d73063f49522903b814fa53951b2ba17f057882b
Arsh <arshgangani18@gmail.com>
2023-04-29 17:32:54 +0530

    Navbar set

--- welcome.php
+++ welcome.php
@@ -1,12 +1,33 @@
+<?php
+
+session_start();
+
+if(!isset($_SESSION['loggedin']) || $_SESSION['loggedin'] !==true)
+{
+    header("location: login.php");
+}
+
+
+?>
 <!DOCTYPE html>
 <html>
 
 <head>
     <title>To-Do List</title>
-    <link rel="stylesheet" type="text/css" href="welcome.css"> 
+    <link rel="stylesheet" href="welcome.css"> 
 </head>
 
 <body>
+<div class="navbar">
+    <nav>
+      <a href="register.php">Register</a>
+      <a href="login.php">Login</a>
+      <a href="logout.php">logout</a>
+      <div class="topright">
+      <a href=""><?php echo "Welcome ". $_SESSION['username']?></a>
+      </div>
+    </nav>
+  </div>
     <div class="todo-container">
         <h1>To-Do List</h1>
         <input type="text" id="new-task" placeholder="Add a new task...">
