05a548a596817897e17eaa8f3fb22efc64ba3d88
Nisarg155 <114862611+Nisarg155@users.noreply.github.com>
2023-05-17 17:21:26 +0530

    validated register.php

--- register.php
+++ register.php
@@ -1,43 +1,34 @@
-
 <?php
 
-require_once"config.php";
+require_once "config.php";
 
 $Email_id = $username = $password  = $confirm_password = "";
 $Email_id_err = $username_err = $password_err  = $confirm_password_err = "";
 
-if($_SERVER['REQUEST_METHOD'] == 'POST')    //todo CHECK THE USERNAME
+if ($_SERVER['REQUEST_METHOD'] == 'POST')    //todo CHECK THE USERNAME
 {
 
     //* CHECK EMAIL
-    if(empty(trim($_POST['Email_id'])))
-    {
-        $Email_id_err = "EMAIL ID CANT BE EMPTY";
-    }
-    else{
+    if (empty(trim($_POST['Email_id']))) {
+        $Email_id_err = "Email can't be empty ";
+    } else {
         $sql = "SELECT id FROM user WHERE Email_id = ? ";
-        $stmt = mysqli_prepare($link,$sql);
+        $stmt = mysqli_prepare($link, $sql);
 
-        if($stmt)
-        {
-            mysqli_stmt_bind_param($stmt,"s",$param_Email_id);
+        if ($stmt) {
+            mysqli_stmt_bind_param($stmt, "s", $param_Email_id);
             $param_Email_id = trim($_POST['Email_id']);
-            if(mysqli_stmt_execute($stmt))
-            {
+            if (mysqli_stmt_execute($stmt)) {
                 mysqli_stmt_store_result($stmt);
-                if(mysqli_stmt_num_rows($stmt) == 1)
-                {
-                    $Email_id_err = "EMAIL ID IS ALREADY TAKEN";
-                }
-                else{
+                if (mysqli_stmt_num_rows($stmt) == 1) {
+                    $Email_id_err = "Email is already taken";
+                } else {
                     $Email_id = trim($_POST['Email_id']);
                 }
-            }
-            else{
+            } else {
                 echo "oops something went wrong";
             }
-        }
-        else{
+        } else {
             echo "oops something went wrong";
         }
         mysqli_stmt_close($stmt);
@@ -48,36 +39,29 @@ if($_SERVER['REQUEST_METHOD'] == 'POST')    //todo CHECK THE USERNAME
 
 
     // * CHECK IF USERNAME IS EMPTY
-    if(empty(trim($_POST['username'])))
-    {
-        $username_err = "USERNAME CANNOT BE BLANK";
-    }
-    else{
+    if (empty(trim($_POST['username']))) {
+        $username_err = "Username can't be blank";
+    } else {
         $sql = "SELECT id FROM user WHERE username = ?";
-        $stmt = mysqli_prepare($link,$sql);
+        $stmt = mysqli_prepare($link, $sql);
 
-        if($stmt)
-        {
-            mysqli_stmt_bind_param($stmt,"s",$param_username);
-            
+        if ($stmt) {
+            mysqli_stmt_bind_param($stmt, "s", $param_username);
+
             // * SET THE VALUE OF PARAMETER USERNAME
             $param_username = trim($_POST['username']);
 
             //* execute the code
 
-            if(mysqli_stmt_execute($stmt))
-            {
+            if (mysqli_stmt_execute($stmt)) {
                 mysqli_stmt_store_result($stmt);
-                if(mysqli_stmt_num_rows($stmt) == 1)
-                {
-                    $username_err = "THIS USERNAME IS ALREADY TAKEN ";
-                }
-                else{
+                if (mysqli_stmt_num_rows($stmt) == 1) {
+                    $username_err = "This Username is already taken";
+                } else {
                     $username = trim($_POST['username']);
                 }
             }
-        }
-        else{
+        } else {
             echo "something went wrong";
         }
         mysqli_stmt_close($stmt);
@@ -87,47 +71,40 @@ if($_SERVER['REQUEST_METHOD'] == 'POST')    //todo CHECK THE USERNAME
 
 
 
-if(empty(trim($_POST['password'])))     //todo  CHECK THE PASSWORD
-{
-    $password_err = "PASSWORD CANNOT BE BLANK";
-}
-else if(strlen(trim($_POST['password'])) < 8){
-
-    $password_err = "PASSWORD LENGTH MUST BE GREATER THAN 8";
-}
-else{
-    $password = trim($_POST['password']);
-}
-
-
-if(trim($_POST['password']) != trim($_POST['confirm_password'])) //TODO CHECK CONFIRM PASSWORD
-{
-    $confirm_password_err = "PASSWORD DOES NOT MATCH";
-}
-
-if(empty($username_err) && empty($password_err) && empty($confirm_password_err) && empty($Email_id_err))
-{
-    $sql = "INSERT INTO user (username,password,Email_id) VALUES (?,?,?)";
-    $stmt = mysqli_prepare($link,$sql);
-    if($stmt)
+    if (empty(trim($_POST['password'])))     //todo  CHECK THE PASSWORD
     {
-        mysqli_stmt_bind_param($stmt,"sss",$param_username,$param_password,$param_Email_id);
-        $param_username = $username;
-        $param_password = password_hash($password,PASSWORD_DEFAULT);
-        $param_Email_id = $Email_id;
-        if(mysqli_stmt_execute($stmt))
-        {
-            header("location: login.php");
-        }
-        else{
-            echo "something went wrong ...cannot redirect!!";
-        }
-    } 
-    mysqli_stmt_close($stmt);
-}
+        $password_err = "please enter password";
+    } else if (strlen(trim($_POST['password'])) < 8) {
 
-mysqli_close($link);
+        $password_err = "password must be greater then length of 8";
+    } else {
+        $password = trim($_POST['password']);
+    }
 
+
+    if (trim($_POST['password']) != trim($_POST['confirm_password'])) //TODO CHECK CONFIRM PASSWORD
+    {
+        $confirm_password_err = "PASSWORD does't match..";
+    }
+
+    if (empty($username_err) && empty($password_err) && empty($confirm_password_err) && empty($Email_id_err)) {
+        $sql = "INSERT INTO user (username,password,Email_id) VALUES (?,?,?)";
+        $stmt = mysqli_prepare($link, $sql);
+        if ($stmt) {
+            mysqli_stmt_bind_param($stmt, "sss", $param_username, $param_password, $param_Email_id);
+            $param_username = $username;
+            $param_password = password_hash($password, PASSWORD_DEFAULT);
+            $param_Email_id = $Email_id;
+            if (mysqli_stmt_execute($stmt)) {
+                header("location: login.php");
+            } else {
+                echo "something went wrong ...cannot redirect!!";
+            }
+        }
+        mysqli_stmt_close($stmt);
+    }
+
+    mysqli_close($link);
 }
 
 ?>
@@ -180,7 +157,8 @@ mysqli_close($link);
             <div class="form-row">
                 <div class="form-group col-md-6">
                     <label for="inputEmail4">Email</label>
-                    <input type="email" class="form-control" id="inputEmail4" name="Email_id"  placeholder="Email">
+                    <input type="email" class="form-control" id="inputEmail4" name="Email_id" placeholder="Email">
+                    <small><?php echo $Email_id_err ?? '' ?></small>
                 </div>
             </div>
 
@@ -188,6 +166,7 @@ mysqli_close($link);
                 <div class="form-group col-md-6">
                     <label for="username">User Name</label>
                     <input type="text" class="form-control" name="username" id="username">
+                    <small><?php echo $username_err ?? '' ?></small>
                 </div>
             </div>
             <div class="form-row">
@@ -201,13 +180,14 @@ mysqli_close($link);
                 </div>
             </div>
             <div class="form-group col-md-6">
-                    <label for="inputPassword">Password</label>
-                    <input type="password" class="form-control" name="password" id="inputPassword" placeholder="Password">
-                </div>
-                <div class="form-group col-md-6">
-                    <label for="inputPassword4">Confirm Password</label>
-                    <input type="password" class="form-control" name="confirm_password"  id="inputPassword4" placeholder="Confirm Password">
-                </div>
+                <label for="inputPassword">Password</label>
+                <input type="password" class="form-control" name="password" id="inputPassword" placeholder="Password">
+                <small><?php echo $password_err ?? '' ?></small>
+            </div>
+            <div class="form-group col-md-6">
+                <label for="inputPassword4">Confirm Password</label>
+                <input type="password" class="form-control" name="confirm_password" id="inputPassword4" placeholder="Confirm Password">
+            </div>
             <div class="form-group">
                 <div class="form-check">
                     <input class="form-check-input" type="checkbox" id="gridCheck">
